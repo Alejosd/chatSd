0d51923345ebc6db787b9167ed4c06bf4e0857df
Alejosd <alejosd5@gmail.com>
2016-10-02 19:27:21 -0300

    config heroku

--- main.go
+++ main.go
@@ -5,20 +5,17 @@ import (
 	"net/http"
 	"chat"
 	"os"
-	"fmt"
 )
 
 func main() {
 
 	port := os.Getenv("PORT")
 
-	fmt.Println("Este es el puerto final heroku:"+port)
-
 	if port == "" {
 		//log.Fatal("$PORT must be set")
 		port="8080"
 	}
-
+	log.Println("PUERTO FINAL:"+port)
 	log.SetFlags(log.Lshortfile)
 
 	// websocket server
